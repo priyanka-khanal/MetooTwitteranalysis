572e42238cc9b86cf1780238165a88b972b1a399
Priyanka Khanal <s524702@nwmissouri.edu>
2019-03-08 00:41:04 -0600

    started

---
+++ twitter_keys.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 28 08:11:10 2019
+
+@author: squishy
+"""
+
+consumer_key = 'ZWQCKrPiKLG7qXBcPMW80LlDO'
+consumer_secret = '1Wtn9o7E696mLib7P4S7jemCHaYUtp6wVLAUetqNaIgYdnTbk0'
+access_token = '1087703281168773121-2LHQhIZuvZ4mvi8LHJ6HUKB7osaIL2'
+access_secret = 'Aog0QMkoyEWi2zYuAEHCWNHoficQT4YHy2qqTjsHbjXpI'
